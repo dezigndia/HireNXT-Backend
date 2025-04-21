7381f353e6aa7ef515189ee2aa23c1ce5ed9a191
starbal123 <sripadbal@gmail.com>
2025-04-21 22:46:01 +0530

    Talent profile backend API

--- src/utils/database.util.ts
+++ src/utils/database.util.ts
@@ -1,9 +1,16 @@
-import { selectQuery } from '../config/database';
+import { selectQuery, query } from '../config/database';
 
 export class databaseUtil {
     public getDropDownValues = async () => {
         const result = await selectQuery('select value from config.drop_down_values where key');
         return result;
     };
+    public addTalentProfile = async (name: string, role: string, skills: string, experienceMonths: string, rate: string, experienceYears: string, notice: string, organization: string, location: string) => {
+        const result = await query(
+            'INSERT INTO talent.talent_profile (name, role, skills, experienceMonths, rate, experienceYears, notice, organization, location) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+      [name, role, skills, experienceMonths, rate, experienceYears, notice, organization, location]
+        );
+        return result;
+    }
 
 }
--- src/utils/fileUpload.ts
+++ src/utils/fileUpload.ts
@@ -1,16 +1,64 @@
 import { Request, Response } from 'express';
+//import multer from 'multer';
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
 
-//dotenv.config();
+
+dotenv.config();
+
+// Configure AWS SDK
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+// const storage = multer.memoryStorage(); // Use memoryStorage to keep the file in memory
+// const upload = multer({ storage });
+
+
+
 export class fileUpload {
 
 public constructor() {}
 
 public uploadFile = async (req: Request, res: Response) => {
 
+  type UploadedFiles = {
+    [fieldname: string]: Express.Multer.File[];
+  };
   try {
-    res.json();
+    if (!req.files) {
+      return res.status(400).json({ message: 'No file uploaded.' });
+    }
+
+    const files = req.files as UploadedFiles;
+    for (const key in files) {
+      const file = files[key][0];
+      
+      const directory = 'Sripad'; 
+
+      const uploadParams: AWS.S3.PutObjectRequest = {
+        Bucket: process.env.S3_BUCKET!,
+        Key: `${directory}/${key}_${Date.now()}`,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: 'public-read',
+      };
+  
+    try {
+      // Upload the file to S3
+      const data = await s3.upload(uploadParams).promise();
+      console.log(`File uploaded successfully. ${data.Location}`);
+    } catch (error) {
+      console.error('Error uploading to S3:', error);
+      return res.status(500).json({ message: 'Error uploading file to S3.' });
+    }
+  }
   } catch (error) {
-    res.json();
+    res.json(error);
   }
 };
 
