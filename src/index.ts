56b6f8457179b12e2f67aa63075980f1a3c2bbfe
Dezigndia Admin <63443015+dezadmin@users.noreply.github.com>
2025-09-11 15:29:38 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { RestServer } from './server';
-new RestServer().startServer(Number(process.env.PORT), process.env.ENV || 'development');
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+if (!port && isNaN(port)) {
+  console.error("❌ Invalid port number! Falling back to 3000.");
+}
+
+new RestServer().startServer(port, process.env.ENV || 'development');
